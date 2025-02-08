ffb7aae4fdf32b5ebc244fcbc68c2aea041c5945
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-02-08 20:50:06 +0530

    add github repo link

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,19 @@ function App() {
   return (
     <div className="App h-screen min-w-screen pb-20 overflow-x-hidden bg-slate-900  ">
       <header className="py-4 px-8 w-full">
-        <h1 className="text-center text-4xl text-white font-bold">User Profile Cards</h1>
+        <h1 className="text-center text-4xl text-white font-bold">
+          User Profile Cards
+        </h1>
         <div className="flex">
           <div className="ml-auto rounded-md text-slate-50 text-right mr-8 text-2xl px-8 py-4 font-semibold bg-blue-300/70 backdrop:blur">
-            Made By: <span>Aditya Mishra</span>
+            Made By:{" "}
+            <a
+              href="https://github.com/AdityaNath0777/random_user_cards"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Aditya Mishra
+            </a>
           </div>
         </div>
       </header>
